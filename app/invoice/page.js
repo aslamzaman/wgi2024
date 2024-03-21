88111363099a8be650ecdef176a8b911acaac43d
aslam <aslamcmes@gmail.com>
2024-03-21 15:20:50 +0600

    Print file added

--- app/invoice/page.js
+++ app/invoice/page.js
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Add from "@/components/invoice/Add";
-import Edit from "@/components/invoice/Edit";    
+import Edit from "@/components/invoice/Edit";
 import Delete from "@/components/invoice/Delete";
+import Print from "@/components/invoice/Print";
+const date_format = dt => new Date(dt).toISOString().split('T')[0];
 
 
 const Invoice = () => {
@@ -43,19 +45,18 @@ const Invoice = () => {
         <>
             <div className="w-full my-6 lg:my-8">
                 <h1 className="w-full text-xl lg:text-3xl font-bold text-center text-blue-700">Invoice</h1>
-            </div>    
+            </div>
             <div className="px-4 lg:px-6">
-                <p className="w-full text-sm text-red-700">{msg}</p>    
+                <p className="w-full text-sm text-red-700">{msg}</p>
                 <table className="w-full border border-gray-200">
                     <thead>
-                        <tr className="w-full bg-gray-200">                           
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Dt</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Invoiceno</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Shipment</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Customer</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Item</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Deduct</th>
-                                  <th className="text-center border-b border-gray-200 px-4 py-2">Payment</th>                                
+                        <tr className="w-full bg-gray-200">
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Date</th>
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Invoice No</th>
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Shipment</th>
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Customer</th>
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Deduct</th>
+                            <th className="text-center border-b border-gray-200 px-4 py-2">Payment</th>
                             <th className="w-[100px] font-normal">
                                 <div className="w-full flex justify-end py-0.5 pr-4">
                                     <Add message={messageHandler} />
@@ -64,23 +65,22 @@ const Invoice = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {invoices.length ?(
+                        {invoices.length ? (
                             invoices.map(invoice => (
-                                <tr className="border-b border-gray-200 hover:bg-gray-100" key={invoice._id}>                                           
-                                          <td className="text-center py-2 px-4">{invoice.dt}</td>
-                                          <td className="text-center py-2 px-4">{invoice.invoiceno}</td>
-                                          <td className="text-center py-2 px-4">{invoice.shipment}</td>
-                                          <td className="text-center py-2 px-4">{invoice.customer}</td>
-                                          <td className="text-center py-2 px-4">{invoice.item}</td>
-                                          <td className="text-center py-2 px-4">{invoice.deduct}</td>
-                                          <td className="text-center py-2 px-4">{invoice.payment}</td>                                            
+                                <tr className="border-b border-gray-200 hover:bg-gray-100" key={invoice._id}>
+                                    <td className="text-center py-2 px-4">{date_format(invoice.dt)}</td>
+                                    <td className="text-center py-2 px-4">{invoice.invoiceno}</td>
+                                    <td className="text-center py-2 px-4">{invoice.shipment}</td>
+                                    <td className="text-center py-2 px-4">{invoice.customer.name}</td>
+                                    <td className="text-center py-2 px-4">{invoice.deduct}</td>
+                                    <td className="text-center py-2 px-4">{invoice.payment}</td>
                                     <td className="flex justify-end items-center space-x-1 mt-1 mr-2">
-                                        <Edit message={messageHandler} id={invoice._id} data={invoices} />
                                         <Delete message={messageHandler} id={invoice._id} data={invoices} />
+                                        <Print message={messageHandler} id={invoice._id} data={invoices} />
                                     </td>
                                 </tr>
                             ))
-                        ): (
+                        ) : (
                             <tr>
                                 <td colSpan={8} className="text-center py-10 px-4">
                                     Data not available.
--- components/invoice/Add.js
+++ components/invoice/Add.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { TextEn, BtnSubmit, BtnSubmitSm, TextDt, TextEnDisabled, DropdownEn } from "@/components/Form";
+import { TextEn, BtnSubmit, BtnSubmitSm, TextDt, TextEnDisabled, DropdownEn, TextNum } from "@/components/Form";
 import { addItem, getItems, deleteItem } from "@/lib/utils/LocalDatabase";
 import { Close } from "../Icons";
 import { fetchData } from "@/lib/utils/FetchData";
@@ -52,7 +52,7 @@ const Add = ({ message }) => {
                 fetchData(`${process.env.NEXT_PUBLIC_BASE_URL}/api/item`),
                 fetchData(`${process.env.NEXT_PUBLIC_BASE_URL}/api/unittype`)
             ]);
-console.log(responseCustomer, responseItem, responseUnittype)
+            console.log(responseCustomer, responseItem, responseUnittype)
             setCustomers(responseCustomer);
             setItems(responseItem);
             setUnittypes(responseUnittype);
@@ -72,12 +72,14 @@ console.log(responseCustomer, responseItem, responseUnittype)
 
 
     const createObject = () => {
+        const response = getItems('invoice');
+        const customerSplit = customer.split(";");
         return {
             dt: dt,
             invoiceno: invoiceno,
             shipment: shipment,
-            customer: customer,
-            item: item,
+            customer: {name: customerSplit[0], address: customerSplit[1], contact: customerSplit[2] },
+            item: response.data,
             deduct: deduct,
             payment: payment
         }
@@ -88,6 +90,7 @@ console.log(responseCustomer, responseItem, responseUnittype)
         e.preventDefault();
         try {
             const newObject = createObject();
+           // console.log(newObject); return false;
             const apiUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/invoice`;
             const requestOptions = {
                 method: "POST",
@@ -120,7 +123,6 @@ console.log(responseCustomer, responseItem, responseUnittype)
         }
         getLocalData();
     }, [msg]);
-
 
     const itemAddHandler = (e) => {
         e.preventDefault();
@@ -167,85 +169,88 @@ console.log(responseCustomer, responseItem, responseUnittype)
                                 </svg>
                             </button>
                         </div>
+
+
                         <div className="px-6 pb-6 text-black">
 
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="grid grid-cols-1 gap-1 my-1">
-                                    <TextDt Title="Date" Id="dt" Change={e => setDt(e.target.value)} Value={dt} />
-                                    <TextEnDisabled Title="Invoice No (Auto)" Id="invoiceno" Change={e => setInvoiceno(e.target.value)} Value={invoiceno} Chr={50} />
-                                    <TextEn Title="Shipment" Id="shipment" Change={e => setShipment(e.target.value)} Value={shipment} Chr={50} />
-                                    <TextEn Title="Customer" Id="customer" Change={e => setCustomer(e.target.value)} Value={customer} Chr={50} />
-                                    <TextEn Title="Item" Id="item" Change={e => setItem(e.target.value)} Value={item} Chr={50} />
-                                    <TextEn Title="Deduct" Id="deduct" Change={e => setDeduct(e.target.value)} Value={deduct} Chr={50} />
-                                    <TextEn Title="Payment" Id="payment" Change={e => setPayment(e.target.value)} Value={payment} Chr={50} />
-                                </div>
-
-                                <div className="col-span-2">
-                                    <p className="w-full text-start text-xs text-blue-400 mt-1">{msg}</p>
-                                    <form onSubmit={itemAddHandler}>
-                                        <div className="grid grid-cols-6 gap-4 mb-4">
-                                            <div className="col-span-2">
-                                                <DropdownEn Title="Item" Id="newItem" Change={e => setNewItem(e.target.value)} Value={newItem}>
-                                                    {items.length ? items.map(item => <option value={item.name} key={item._id}>{item.name}</option>) : null}
-                                                </DropdownEn>
-                                            </div>
-                                            <TextEn Title="Qty" Id="newQty" Change={e => setNewQty(e.target.value)} Value={newQty} Chr={50} />
-                                            <DropdownEn Title="Unit" Id="newUnit" Change={e => setNewUnit(e.target.value)} Value={newUnit}>
-                                                {unittypes.length ? unittypes.map(unittype => <option value={unittype.name} key={unittype._id}>{unittype.name}</option>) : null}
+                            <div className="w-full p-1 overflow-auto">
+                                <p className="w-full text-start text-xs text-blue-400 mt-1">{msg}</p>
+                                <form onSubmit={itemAddHandler}>
+                                    <div className="grid grid-cols-4 gap-2 bg-blue-200 p-2">
+                                        <div className="col-span-4">
+                                            <DropdownEn Title="Item" Id="newItem" Change={e => setNewItem(e.target.value)} Value={newItem}>
+                                                {items.length ? items.map(item => <option value={`${item.name};${item.description}`} key={item._id}>{item.name}</option>) : null}
                                             </DropdownEn>
-
-                                            <TextEn Title="Rate" Id="newRate" Change={e => setNewRate(e.target.value)} Value={newRate} Chr={50} />
-                                            <BtnSubmitSm Title="Save" Class="h-8 mt-5 bg-blue-600 hover:bg-blue-800 text-white" />
                                         </div>
-                                    </form>
+                                        <TextEn Title="Qty" Id="newQty" Change={e => setNewQty(e.target.value)} Value={newQty} Chr={50} />
+                                        <DropdownEn Title="Unit" Id="newUnit" Change={e => setNewUnit(e.target.value)} Value={newUnit}>
+                                            {unittypes.length ? unittypes.map(unittype => <option value={unittype.name} key={unittype._id}>{unittype.name}</option>) : null}
+                                        </DropdownEn>
+                                        <TextEn Title="Rate" Id="newRate" Change={e => setNewRate(e.target.value)} Value={newRate} Chr={50} />
+                                        <BtnSubmitSm Title="Add Item" Class="h-9 mt-5 bg-blue-600 hover:bg-blue-800 text-white" />
+                                    </div>
+                                </form>
+                            </div>
+                            
+                            <div className="w-full p-1 overflow-auto">
+                                <table className="w-full border border-gray-100 mt-1">
+                                    <thead>
+                                        <tr className="w-full bg-gray-200">
+                                            <th className="text-start border-b border-gray-200 px-4 py-2">Item</th>
+                                            <th className="text-center border-b border-gray-200 px-4 py-2">Rate</th>
+                                            <th className="text-center border-b border-gray-200 px-4 py-2">Unit</th>
+                                            <th className="text-center border-b border-gray-200 px-4 py-2">Qty</th>
+                                            <th className="w-[80px] text-end border-b border-gray-200 px-4 py-2">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {locals.length ? (
+                                            locals.map(itm => (
+                                                <tr className="border-b border-gray-200 hover:bg-gray-100" key={itm.id}>
+                                                    <td className="text-start py-2 px-4 line-clamp-4">{itm.name}</td>
+                                                    <td className="text-center py-2 px-4">{itm.rate}</td>
+                                                    <td className="text-center py-2 px-4 line-clamp-2">{itm.unit}</td>
+                                                    <td className="text-center py-2 px-4">{itm.qty}</td>
 
-                                    <table className="w-full border border-gray-200 mt-10">
-                                        <thead>
-                                            <tr className="w-full bg-gray-200">
-                                                <th className="text-start border-b border-gray-200 px-4 py-2">Item</th>
-                                                <th className="text-center border-b border-gray-200 px-4 py-2">Rate</th>
-                                                <th className="text-center border-b border-gray-200 px-4 py-2">Unit</th>
-                                                <th className="text-center border-b border-gray-200 px-4 py-2">Qty</th>
-                                                <th className="w-[80px] text-end border-b border-gray-200 px-4 py-2">
-                                                    Action
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {locals.length ? (
-                                                locals.map(itm => (
-                                                    <tr className="border-b border-gray-200 hover:bg-gray-100" key={itm.id}>
-                                                        <td className="text-start py-2 px-4 line-clamp-4">{itm.name}</td>
-                                                        <td className="text-center py-2 px-4">{itm.rate}</td>
-                                                        <td className="text-center py-2 px-4 line-clamp-2">{itm.unit}</td>
-                                                        <td className="text-center py-2 px-4">{itm.qty}</td>
-
-                                                        <td className="flex justify-end items-center space-x-1 mt-1 mr-2">
-                                                            <Close Click={() => deleteHookHandler(itm.id)} Size="w-6 h-6" />
-                                                        </td>
-                                                    </tr>
-                                                ))
-                                            ) : (
-                                                <tr>
-                                                    <td colSpan={8} className="text-center py-10 px-4">
-                                                        Data not available.
+                                                    <td className="flex justify-end items-center space-x-1 mt-1 mr-2">
+                                                        <Close Click={() => deleteHookHandler(itm.id)} Size="w-6 h-6" />
                                                     </td>
                                                 </tr>
-                                            )}
-                                        </tbody>
-                                    </table>
-
-
-
-                                </div>
-
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan={8} className="text-center py-10 px-4">
+                                                    Data not available.
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
                             </div>
 
+                            <div className="w-full p-1 overflow-auto">
+                                <form onSubmit={saveHandler}>
+                                    <div className="grid grid-cols-3 gap-3 my-1 bg-pink-200 p-2">
+                                        <TextDt Title="Date" Id="dt" Change={e => setDt(e.target.value)} Value={dt} />
+                                        <TextEnDisabled Title="Invoice No (Auto)" Id="invoiceno" Change={e => setInvoiceno(e.target.value)} Value={invoiceno} Chr={50} />
+                                        <TextNum Title="Shipment" Id="shipment" Change={e => setShipment(e.target.value)} Value={shipment} />
+                                        <DropdownEn Title="Customer" Id="customer" Change={e => setCustomer(e.target.value)} Value={customer}>
+                                            {customers.length ? customers.map(customer => <option value={`${customer.name};${customer.address};${customer.contact}`} key={customer._id}>{customer.name}</option>) : null}
+                                        </DropdownEn>
+
+                                        {/* <TextEn Title="Item" Id="item" Change={e => setItem(e.target.value)} Value={item} Chr={50} /> */}
+                                        <TextNum Title="Deduct" Id="deduct" Change={e => setDeduct(e.target.value)} Value={deduct} />
+                                        <TextNum Title="Payment" Id="payment" Change={e => setPayment(e.target.value)} Value={payment} />
+                                    </div>
 
 
-                            <div className="w-full flex justify-start">
-                                <input type="button" onClick={closeAddForm} value="Close" className="bg-pink-600 hover:bg-pink-800 text-white text-center mt-3 mx-0.5 px-4 py-2 font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300 cursor-pointer" />
-
+                                    <div className="w-full flex justify-start">
+                                        <input type="button" onClick={closeAddForm} value="Close" className="bg-pink-600 hover:bg-pink-800 text-white text-center mt-3 mx-0.5 px-4 py-2 font-semibold rounded-md focus:ring-1 ring-blue-200 ring-offset-2 duration-300 cursor-pointer" />
+                                        <BtnSubmit Title="Save" Class="bg-blue-600 hover:bg-blue-800 text-white" />
+                                    </div>
+                                </form>
                             </div>
 
                         </div>
